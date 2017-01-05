e1814d30c5c9ae61a9bada69d0329516e9a57988
rubencr1979 <rubencr1979@gmail.com>
2017-01-05 15:28:27 +0100

    iepaaa Git!

---
+++ MvcMusicStore/Models/Album.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcMusicStore.Models
+{
+    class Album
+    {
+        public int AlbumID { get; set; }
+
+        public string Name { get; set; }
+
+        public int ArtistID { get; set; }
+
+        public string Year { get; set; }
+    }
+}
